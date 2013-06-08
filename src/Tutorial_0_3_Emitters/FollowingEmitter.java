0ffc2b10de5b7e6c1f345e0f9d1117f6a25b1003
robmadeyou <robmadeyou@gmail.com>
2013-06-08 10:39:45 +0100

    Fix'd

---
+++ src/Tutorial_0_3_Emitters/FollowingEmitter.java
@@ -0,0 +1,43 @@
+package Tutorial_0_3_Emitters;
+
+import com.gmail.robmadeyou.Screen;
+import com.gmail.robmadeyou.Effects.Color;
+import com.gmail.robmadeyou.Effects.Emitter;
+import com.gmail.robmadeyou.Entity.EntityList;
+import com.gmail.robmadeyou.Entity.Player;
+import com.gmail.robmadeyou.Input.Mouse;
+import com.gmail.robmadeyou.Screen.GameType;
+
+public class FollowingEmitter {
+
+	public static void main(String[] args) {
+
+		Screen.create(640, 640, "AI_Demo", GameType.SIDE_SCROLLER, false);
+		
+		Player player = new Player(40, 40, 32, 64);
+		EntityList.addEntity(player);
+		
+		Emitter emitter = new Emitter(Mouse.getX(), Mouse.getY(), 2000, 0.02f, 0.2f, 2, Color.Blue);
+		/*
+		 * As most objects in the game engine, they have to be added to the update 
+		 * list, without this the emitter would be created but nothing would get
+		 * rendered
+		 */
+		Emitter.addNewEmitter(emitter);
+		
+		Screen.setUpWorld();
+		while(!Screen.isAskedToClose()){
+			Screen.update(60);
+			
+			/*
+			 * Here we set the location of the emitter to the location of the mouse
+			 * this way it looks like the mouse has a trail following it
+			 */
+			emitter.setX(Mouse.getX());
+			emitter.setY(Mouse.getY());
+			
+			Screen.refresh();
+		}
+		Screen.destroy();
+	}
+}
---
+++ src/Tutorial_0_4_Layering/LayeringDemo.java
@@ -0,0 +1,58 @@
+package Tutorial_0_4_Layering;
+
+import com.gmail.robmadeyou.Layer;
+import com.gmail.robmadeyou.Screen;
+import com.gmail.robmadeyou.Effects.Color;
+import com.gmail.robmadeyou.Effects.Emitter;
+import com.gmail.robmadeyou.Effects.TextureLoader;
+import com.gmail.robmadeyou.Entity.Enemy;
+import com.gmail.robmadeyou.Entity.EntityList;
+import com.gmail.robmadeyou.Entity.Player;
+import com.gmail.robmadeyou.Screen.GameType;
+
+public class LayeringDemo {
+	
+	public static void main(String[] args) {
+
+		Screen.create(640, 640, "AI_Demo", GameType.SIDE_SCROLLER, false);
+		
+		Player player = new Player(40, 40, 32, 64);
+		EntityList.addEntity(player);
+		/*
+		 * By default there are 2 layers at the start
+		 * 0 and 1. 
+		 * To add more layers do:
+		 * Layer.add();
+		 * 
+		 * This will make the player be appear after the emitter is rendered
+		 * as it is on a higher layer. The higher the layer the later it will
+		 * be rendered (the more it stands above everyone else)
+		 */
+		player.setLayer(1);
+		/*
+		 * When creating a new texture, an integer is returned so we are able
+		 * to assign it to an integer and name it (this is done so we can re-use the texture)
+		 */
+		int flame = TextureLoader.createTexture("flame.png");
+		
+		Emitter emitter = new Emitter(200, 600, 2000, 0.02f, 0.2f, 2, Color.White, flame);
+		Emitter.addNewEmitter(emitter);
+		
+		
+		/*
+		 * Setting custom particle sizes to make the particles being emitted actually display
+		 * a texture
+		 */
+		emitter.setCustomParticleHeight(20);
+		emitter.setCustomParticleWidth(20);
+		
+		Screen.setUpWorld();
+		while(!Screen.isAskedToClose()){
+			Screen.update(60);
+			
+			
+			Screen.refresh();
+		}
+		Screen.destroy();
+	}
+}
